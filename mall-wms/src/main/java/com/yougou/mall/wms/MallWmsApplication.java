dcd2ead94a7816e0ba1d9d7664f719b38b92e98d
cl96 <892057587@qq.com>
2020-04-03 15:49:32 +0800

    generate some modules

---
+++ mall-wms/src/main/java/com/yougou/mall/wms/MallWmsApplication.java
@@ -0,0 +1,13 @@
+package com.yougou.mall.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWmsApplication.class, args);
+    }
+
+}
